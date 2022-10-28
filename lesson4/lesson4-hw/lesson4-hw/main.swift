6735da7756c17f405ef413663b9ac62cb21c96d3
androidc <chizztectep@gmail.com>
2022-10-28 16:10:05 +0300

    lesson4-hw commit

---
+++ lesson4/lesson4-hw/lesson4-hw/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  lesson4-hw
+//
+//  Created by Артем Солохин on 28.10.2022.
+//
+
+import Foundation
+
+enum CarMenu {
+    case engade
+    case windowOpen
+    case move(movingSpace:Int)
+    case new
+}
+
+print("afdaf")
+

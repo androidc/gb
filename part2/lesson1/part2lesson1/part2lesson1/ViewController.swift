bc3d464b2af0b7bd68adfee1d5bd9a644eb297cb
androidc <chizztectep@gmail.com>
2022-11-28 15:04:15 +0300

    part2lesson1

---
+++ part2/lesson1/part2lesson1/part2lesson1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  part2lesson1
+//
+//  Created by Артем Солохин on 28.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

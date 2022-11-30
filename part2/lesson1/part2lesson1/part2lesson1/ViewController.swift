37c6a9b7e6c50db105700b6a9fe5ff3db44c9b5e
androidc <chizztectep@gmail.com>
2022-11-30 20:53:27 +0300

    part2lesson2

--- part2/lesson1/part2lesson1/part2lesson1/ViewController.swift
+++ part2/lesson1/part2lesson1/part2lesson1/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var loginInput: UITextField!
+    
+    @IBOutlet weak var passInput: UITextField!
+    
+    @IBAction func openTabBar(_ sender: Any) {
+        
+        guard let login = loginInput.text, let password = passInput.text, login == "aaa", password == "111" else {
+            show(message: "неверный логин или пароль")
+            return}
+        
+        performSegue(withIdentifier: "openMainTabBar", sender: nil)
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+}
+
+extension UIViewController {
+    
+    func show(message: String) {
+        let alertVC = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        
+        alertVC.addAction(okAction)
+        
+        present(alertVC,animated: true,completion: nil)
+        
+    }
 }
 
---
+++ part2/lesson1/part2lesson1/part2lesson1/table1ViewController.swift
@@ -0,0 +1,10 @@
+//
+//  table1ViewController.swift
+//  part2lesson1
+//
+//  Created by Артем Солохин on 30.11.2022.
+//
+
+import Foundation
+
+
